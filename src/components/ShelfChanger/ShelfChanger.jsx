94b673dd91ebf9ee73d7e8ba3a2818456cbb47b2
Mahmoud Fouad Ali Abo Ali <84210982+mahmoudfouadweb@users.noreply.github.com>
2022-10-07 02:20:53 +0200

    import ShelfChanger

--- src/components/ShelfChanger/ShelfChanger.jsx
+++ src/components/ShelfChanger/ShelfChanger.jsx
@@ -4,7 +4,19 @@ import classes from './shelfChanger.module.scss'
 
 const ShelfChanger = () => {
   return (
-    <div>ShelfChanger</div>
+    <div className="book-shelf-changer">
+    <select>
+      <option value="none" disabled>
+        Move to...
+      </option>
+      <option value="currentlyReading">
+        Currently Reading
+      </option>
+      <option value="wantToRead">Want to Read</option>
+      <option value="read">Read</option>
+      <option value="none">None</option>
+    </select>
+  </div>
   )
 }
 
